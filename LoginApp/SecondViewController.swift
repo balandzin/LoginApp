5c60c34a2627b10ff006968542e1e7e843228b6d
balandzin <balandzin2602@gmail.com>
2024-03-04 20:03:13 +0300

    Добавил SecondViewController

---
+++ LoginApp/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  LoginApp
+//
+//  Created by Антон Баландин on 4.03.24.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
